0b966cf0d71fd2c2d622f6cff35d8714d2119448
MajidGujjar <majigujjar11471@gmail.com>
2026-04-27 14:58:29 +0500

    Improved Canvas.js

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -6,7 +6,7 @@ const Canvas = ({children}) => {
     return (
         <div 
         ref={setNodeRef}
-        className={`min-h-[400px] w-full p-5 rounded-2xl border-2 border-dashed transition-all duration-300 flex flex-col justify-start
+        className={`max-h-[500px] overflow-y-auto w-full p-5 rounded-2xl border-2 border-dashed transition-all duration-300 flex flex-col justify-start
             ${isOver ? "border-blue-500 bg-blue-200 shadow-lg scale-[1.01]" : "border-gray-300 bg-white"}`}>
                 {children}
             </div>
